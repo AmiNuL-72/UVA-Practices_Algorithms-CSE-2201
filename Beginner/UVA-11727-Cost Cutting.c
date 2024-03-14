c801092dbfd3e6fd360d9053684be1d922a1d086
AmiNuL-72 <157211474+AmiNuL-72@users.noreply.github.com>
2024-03-14 16:45:37 +0600

    Update UVA-11727-Cost Cutting.c

--- Beginner/UVA-11727-Cost Cutting.c
+++ Beginner/UVA-11727-Cost Cutting.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+int main(){
+    int t,i;
+    scanf("%d",&t);
+    for(i=1; i<=t; i++){
+        int x,y,z;
+        scanf("%d %d %d",&x,&y,&z);
+        int ans;
+        if((x>y && x<z) || (x<y && x>z)) ans=x;
+        else if((y>x && y<z) || (y<x && y>z)) ans=y;
+        else ans=z;
 
+        printf("Case %d: %d\n",i,ans);
+    }
+    return 0;
+}
