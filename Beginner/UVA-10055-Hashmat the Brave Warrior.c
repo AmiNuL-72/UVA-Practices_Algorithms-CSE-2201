2872fed34d6532d7b0f6664fd77b1870e242d00e
AmiNuL-72 <157211474+AmiNuL-72@users.noreply.github.com>
2024-03-14 16:02:47 +0600

    Update UVA-10055-Hashmat the Brave Warrior.c

--- Beginner/UVA-10055-Hashmat the Brave Warrior.c
+++ Beginner/UVA-10055-Hashmat the Brave Warrior.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main(){
+    long long int a,b;
+    while(scanf("%lld %lld",&a,&b) != EOF){
+        long long int dif;
+        if(b>=a) dif=b-a;
+        else dif=a-b;
+        printf("%lld\n",dif);
+    }
+    return 0;
+}
