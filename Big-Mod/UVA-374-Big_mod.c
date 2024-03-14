8ee27d46cb9321d150cd983b25a06a62aac5f59d
AmiNuL-72 <157211474+AmiNuL-72@users.noreply.github.com>
2024-03-15 05:53:08 +0600

    Update UVA-374-Big_mod.c

--- Big-Mod/UVA-374-Big_mod.c
+++ Big-Mod/UVA-374-Big_mod.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#define ll long long int
 
+ll Big_Mod(ll b, ll p, ll m){
+    if(p==0) return 1;
+    if(p%2==0){
+        ll x=Big_Mod(b,p/2,m);
+        return ((x%m)*(x%m))%m;
+    }
+    else{
+        return ((b%m)*(Big_Mod(b,p-1,m)))%m;
+    }
+}
+int main(){
+    ll base,pow,mod;
+    while(scanf("%lld %lld %lld", &base, &pow, &mod) != EOF){
+        printf("%lld\n",Big_Mod(base,pow,mod));
+    }
+    return 0;
+}
