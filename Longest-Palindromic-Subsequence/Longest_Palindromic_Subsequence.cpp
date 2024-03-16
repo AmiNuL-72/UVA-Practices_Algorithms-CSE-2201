92428757e45719ffcab68c8e6e9dbd2472d8a8d1
AmiNuL-72 <157211474+AmiNuL-72@users.noreply.github.com>
2024-03-16 17:54:39 +0600

    Update Longest_Palindromic_Subsequence.cpp

--- Longest-Palindromic-Subsequence/Longest_Palindromic_Subsequence.cpp
+++ Longest-Palindromic-Subsequence/Longest_Palindromic_Subsequence.cpp
@@ -8,30 +8,28 @@ using namespace std;
 
 int longestPalinSubseq(string S)
 {
-	string R = S;
-	reverse(R.begin(), R.end());
-  int sz=S.size();
-	int dp[sz+1][sz+1];
-    memset(dp, 0, sizeof(dp));
+     string R = S;
+     reverse(R.begin(), R.end());
+     int sz=S.size();
+     int dp[sz+1][sz+1];
+     memset(dp, 0, sizeof(dp));
 
-	for (int i=1; i<=sz; i++) {
-		for (int j=1; j<=sz; j++) {
-			if(S[i-1] == R[j-1])
-				dp[i][j] = 1 + dp[i-1][j-1];
-			else
-				dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
-		}
+     for (int i=1; i<=sz; i++) {
+	for (int j=1; j<=sz; j++) {
+		if(S[i-1] == R[j-1])
+		     dp[i][j] = 1 + dp[i-1][j-1];
+		else
+		     dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
 	}
-  return dp[sz][sz];
+     }
+     return dp[sz][sz];
 }
 
 int main()
 {
-	string s = "GEEKSFORGEEKS";
-  //string s="agbdba";
-	cout << "The length of the LPS is : "
-		<< longestPalinSubseq(s) << endl;
-
-	return 0;
+     string s = "GEEKSFORGEEKS";
+     //string s="agbdba";
+     cout << longestPalinSubseq(s) << endl;
+     return 0;
 }
 
